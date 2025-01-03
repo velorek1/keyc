aa6bee3f7597191334850518415f6181f89ddf78
Raúl Sanguiao <velorek1@gmail.com>
2025-01-03 12:00:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 echo -e "\e[93mKeyCapture - Coded By v3l0r3k\e[0m"
 echo
 echo [+] Attempting to build...
-gcc keyc.c -o keyc -levdev
+echo Check cat /proc/bus/input/devices to determine keyboard event file name
+gcc keyc.c -o keyc 
 echo gcc keyc.c -o keyc
 echo -e "Run as \e[93m./keyc\e[0m" 
 echo Enjoy it!
